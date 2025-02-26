e9f12380206be6b2a93c07eddcd07c46e9ac3dda
chibuike <enechukwuchibuike0@gmail.com>
2025-02-26 23:14:40 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import Button from "./Button";
+
+function App() {
+  return (
+    <>
+      <h1 className="">Hello World</h1>
+      <Button btnText="Click Me" color="white" bg="blue" />
+      <Button btnText="Sign Up" color="white" bg="green" />
+    </>
+
+    // <></> - React Fragment
+  );
+}
+
+export default App;
