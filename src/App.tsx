c71684a0f9d519f09e2234e412814fc541bd1a31
chibuike <enechukwuchibuike0@gmail.com>
2025-03-13 22:48:49 +0100

    props

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
-import Todo from "./Todo";
+import Button from "./Button";
+import Container from "./Container";
 
 function App() {
   return (
-    <main>
-      <Todo />
+    <main className="flex justify-center items-center h-screen">
+      <Container>
+        <Button />
+        <Button />
+        <Button text="Sign Up" />
+      </Container>
     </main>
   );
 }
