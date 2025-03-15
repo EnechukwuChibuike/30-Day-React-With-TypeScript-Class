ffa185c301179b925848493b9d201f83ddb67d97
chibuike <enechukwuchibuike0@gmail.com>
2025-03-15 09:06:34 +0100

    context

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,27 @@
-import Button from "./Button";
-import Container from "./Container";
+import { useTheme } from "./ThemeContext";
 
 function App() {
+  const { theme, toggleTheme } = useTheme();
+
   return (
-    <main className="flex justify-center items-center h-screen">
-      <Container>
-        <Button />
-        <Button />
-        <Button text="Sign Up" />
-      </Container>
+    <main
+      className={`flex h-screen ${theme === "light" ? "bg-white" : "bg-black"}`}
+    >
+      <header
+        className={`flex justify-between items-center h-[10vh] w-full shadow-lg px-10 ${
+          theme === "light" ? "text-black" : "text-white"
+        }}`}
+      >
+        <strong className={theme === "light" ? "text-black" : "text-white"}>
+          Logo
+        </strong>
+        <button
+          className="bg-blue-700 text-white px-3 py-2 hover:brightness-75"
+          onClick={toggleTheme}
+        >
+          {theme}
+        </button>
+      </header>
     </main>
   );
 }
--- src/Container.tsx
+++
@@ -1,9 +0,0 @@
-interface ContainerProps {
-  children: React.ReactNode;
-}
-
-export default function Container({ children }: ContainerProps) {
-  return (
-    <section className="border-[1px] border-black p-20">{children}</section>
-  );
-}
