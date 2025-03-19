19b28c90370c86cda788fa7b6d2a06ef5dd5563b
chibuike <enechukwuchibuike0@gmail.com>
2025-03-19 23:30:20 +0100

    done

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,9 @@
-import { useTheme } from "./ThemeContext";
+import Counter from "./Counter";
 
 function App() {
-  const { theme, toggleTheme } = useTheme();
-
   return (
-    <main className={`${theme === "light" ? "bg-white" : "bg-black"} h-screen`}>
-      <header className="flex justify-between items-center p-4 h-[10vh] shadow-2xl">
-        <strong className={theme === "light" ? "text-black" : "text-white"}>
-          Logo.
-        </strong>
-
-        <button
-          onClick={toggleTheme}
-          className={`px-2 py-3 bg-blue-600 hover:brightness-75 text-white`}
-        >
-          {theme} mode
-        </button>
-      </header>
+    <main className={`h-screen`}>
+      <Counter />
     </main>
   );
 }
