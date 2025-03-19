4e80c85d9e932628dac775d864f3e8f12a952719
chibuike <enechukwuchibuike0@gmail.com>
2025-03-19 23:35:08 +0100

    done

--- src/Counter.tsx
+++ src/Counter.tsx
@@ -47,7 +47,9 @@ export default function Counter() {
 
   return (
     <main
-      className={`flex flex-col items-center justify-center h-screen ${state.theme} === "light" ?  "bg-white" : "bg-black"`}
+      className={`flex flex-col items-center justify-center h-screen ${
+        state.theme === "light" ? "bg-white" : "bg-black"
+      }`}
     >
       <h1 className="text-5xl">{state.count}</h1>
 
