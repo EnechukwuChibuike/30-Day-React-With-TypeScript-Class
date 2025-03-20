ca7029a42b90d426d7928801e6175e6751c337c8
chibuike <enechukwuchibuike0@gmail.com>
2025-03-20 22:58:38 +0100

    react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
-import Counter from "./Counter";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
 
 function App() {
   return (
     <main className={`h-screen`}>
-      <Counter />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
     </main>
   );
 }
